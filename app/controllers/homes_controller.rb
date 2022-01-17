2e06dfc768f36c09965f2e7724f386029e2bb5c7
ryooookada <btbl70120@gmail.com>
2022-01-17 15:07:46 +0000

    practice

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,6 @@
 class HomesController < ApplicationController
   def top
   end
+  def top1
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/top'
+  get 'homes/top1'
   
 end
